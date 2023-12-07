786f094bb12e4a322b672f52c0ace5adae1e15b5
Ryan Moon <74520918+ryan-kanghyun-moon@users.noreply.github.com>
2023-12-07 17:57:42 +0900

    change font size

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Page2 from "./Page2";
 import Nav from "./Nav";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       {/* <div className="App"> */}
       {/* <Nav /> */}
       {/* <Switch> */}
